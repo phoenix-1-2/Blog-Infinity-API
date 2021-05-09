fb602978e54c3f8a20a34751ae76e05df1deeeb8
Nikhil Bhasin <nikhilbhasin@Nikhils-Air.Dlink>
2021-05-09 15:54:36 +0530

    H10 Error Fixed

--- app.js
+++ app.js
@@ -38,30 +38,16 @@ app.use(
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-//   );
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
-const allowedOrigins = ['http://localhost:3000',
-  'https://blog-infinity.netlify.app'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
@@ -82,6 +68,7 @@ mongoose
     useNewUrlParser : true
   })
   .then(result => {
-    app.listen(8080);
+    console.log('Connected To MongoDB');
+    app.listen(8080 || process.env.PORT);
   })
   .catch(err => console.log(err));
